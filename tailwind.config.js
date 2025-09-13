a0466ca0323c9f80d1375c7dfbdbd9d484da981a
abinazebinoy <abinazebinoy@karunya.edu.in>
2025-09-13 15:38:45 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0F172A',
+        accent: '#06B6D4',
+        soft: '#f8fafc'
+      }
+    }
+  },
+  plugins: []
+}
